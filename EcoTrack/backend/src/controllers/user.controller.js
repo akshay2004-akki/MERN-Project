73342c304d647af4166a9ccbfdb0cb78243cda0e
Akshay anand <akshay1622004@gmail.com>
2024-09-23 23:45:40 +0530

    for login

--- EcoTrack/backend/src/controllers/user.controller.js
+++ EcoTrack/backend/src/controllers/user.controller.js
@@ -143,7 +143,7 @@ export const loginUser = asyncHandler(async(req,res)=>{
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
     const options = {
-        httpOnly : true,
+        httpOnly : false,
         secure : true,
         sameSite : "strict",
         // sameSite:"strict"
